e963634430a4f703f71721b503526036b95f641c
ivan.hernandez <ivan.hernandez@dautom.com.co>
2023-01-23 16:53:46 -0500

    browseTagsImpl added to ClientScriptModule
    BUG present, cant return context object

--- scripting-function/scripting-function-client/src/main/java/com/inductiveautomation/ignition/examples/scripting/client/ClientScriptModule.java
+++ scripting-function/scripting-function-client/src/main/java/com/inductiveautomation/ignition/examples/scripting/client/ClientScriptModule.java
@@ -3,6 +3,7 @@ package com.inductiveautomation.ignition.examples.scripting.client;
 import com.inductiveautomation.ignition.examples.scripting.AbstractScriptModule;
 import com.inductiveautomation.ignition.examples.scripting.MathBlackBox;
 import com.inductiveautomation.ignition.client.gateway_interface.ModuleRPCFactory;
+import com.inductiveautomation.ignition.gateway.model.GatewayContext;
 
 public class ClientScriptModule extends AbstractScriptModule {
 
@@ -18,6 +19,11 @@ public class ClientScriptModule extends AbstractScriptModule {
     @Override
     protected int multiplyImpl(int arg0, int arg1) {
         return rpc.multiply(arg0, arg1);
+    }
+
+    @Override
+    protected GatewayContext browseTagsImpl() {
+        return rpc.browseTags();
     }
 
 }
