98a95ab87690e69b4ea52c84947342e35b20dacf
ivan.hernandez <ivan.hernandez@dautom.com.co>
2023-01-23 16:15:13 -0500

    browseTags added to AbstractScriptModule.

--- scripting-function/scripting-function-common/src/main/java/com/inductiveautomation/ignition/examples/scripting/AbstractScriptModule.java
+++ scripting-function/scripting-function-common/src/main/java/com/inductiveautomation/ignition/examples/scripting/AbstractScriptModule.java
@@ -3,6 +3,7 @@ package com.inductiveautomation.ignition.examples.scripting;
 import com.inductiveautomation.ignition.common.BundleUtil;
 import com.inductiveautomation.ignition.common.script.hints.ScriptArg;
 import com.inductiveautomation.ignition.common.script.hints.ScriptFunction;
+import com.inductiveautomation.ignition.gateway.model.GatewayContext;
 
 public abstract class AbstractScriptModule implements MathBlackBox {
 
@@ -23,6 +24,13 @@ public abstract class AbstractScriptModule implements MathBlackBox {
         return multiplyImpl(arg0, arg1);
     }
 
+    @Override
+    @ScriptFunction(docBundlePrefix = "AbstractScriptModule")
+    public GatewayContext browseTags(){
+            return browseTagsImpl();
+    }
+
     protected abstract int multiplyImpl(int arg0, int arg1);
+    protected abstract GatewayContext browseTagsImpl();
 
 }
