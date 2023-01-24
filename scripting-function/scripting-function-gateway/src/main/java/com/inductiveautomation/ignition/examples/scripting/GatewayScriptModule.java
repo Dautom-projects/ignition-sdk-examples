0ae827d5be09a6e38d618db4d3075d9c3d8f6247
WilliamMejiaParodi <william.mejia@dautom.com.co>
2023-01-24 16:15:57 -0500

    deleteTagImpl added GatewayScriptModule

--- scripting-function/scripting-function-gateway/src/main/java/com/inductiveautomation/ignition/examples/scripting/GatewayScriptModule.java
+++ scripting-function/scripting-function-gateway/src/main/java/com/inductiveautomation/ignition/examples/scripting/GatewayScriptModule.java
@@ -72,6 +72,31 @@ public class GatewayScriptModule extends AbstractScriptModule {
             }
         }
     }
+    protected void deleteTagsImpl() throws Exception {
+        GatewayContext context = GatewayHook.getGatewayContext();
+        GatewayTagManager tagManager = context.getTagManager();
+        TagProvider provider = tagManager.getTagProvider("default");  // Change tag provider name here as needed
 
+
+        List<TagPath> toDelete = new ArrayList<>();
+        TagPath levelOne_FolderA = TagPathParser.parse("LevelOne_FolderA");
+        toDelete.add(levelOne_FolderA);
+
+        TagPath tinyUdtOverrideInstance = TagPathParser.parse("TinyUdt_OverrideInstance");
+        toDelete.add(tinyUdtOverrideInstance);
+
+
+        List<QualityCode> results = provider.removeTagConfigsAsync(toDelete)
+                .get(30, TimeUnit.SECONDS);
+
+        for (int i = 0; i < results.size(); i++) {
+            QualityCode result = results.get(i);
+            if (result.isNotGood()) {
+                TagPath tagPath = toDelete.get(i);
+                throw new Exception(String.format("Delete tag operation for tag '%s' returned bad result '%s'",
+                        tagPath.toStringFull(), result.toString()));
+            }
+        }
+    }
 
 }
